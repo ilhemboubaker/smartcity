3968b1de04268594c17f4c206aac6a6e22582469
ilhemboubaker <ilhem.boubaker580@gmail.com>
2024-02-07 11:59:49 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Smart-City',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
